87f96560ee166412ae0c951caa8ce6e56b0dbc1f
salvador <salvador@ssapia.net>
2015-06-27 13:47:00 -0300

    - move o código do totp para o controller do token.

--- www/token/token.js
+++ www/token/token.js
@@ -11,7 +11,7 @@ angular
         controller: 'TokenController as vm'
       });
   })
-  .controller('TokenController', function($interval,UsersModel, ErrorsModel, $location, AuthTokenFactory) {
+  .controller('TokenController', function($interval, UsersModel, ErrorsModel, $location, AuthTokenFactory) {
 
     var vm = this;
 
@@ -31,8 +31,7 @@ angular
         $location.path('/login');
       });
 
-    function getTimer()
-    {
+    function getTimer() {
       var epoch = Math.round(new Date().getTime() / 1000.0);
       var countDown = 30 - (epoch % 30);
       if (epoch % 30 == 0) getOtp();
@@ -41,4 +40,53 @@ angular
       //$('.progress-bar').css('width', percent+'%').attr('aria-valuenow', countDown);
       return countDown;
     }
- });
+
+    function getOtp() {
+
+      /* A chave será retornada no response do login */
+      var secret	= "JBSWY3DPEHPK3PXP";
+      /***********************************************/
+
+      var key = base32tohex(secret);
+      var epoch = Math.round(new Date().getTime() / 1000.0);
+      var time = leftpad(dec2hex(Math.floor(epoch / 30)), 16, '0');
+
+      var hmacObj = new jsSHA(time, 'HEX');
+      var hmac = hmacObj.getHMAC(key, 'HEX', 'SHA-1', "HEX");
+
+      var offset = hex2dec(hmac.substring(hmac.length - 1));
+
+      var otp = (hex2dec(hmac.substr(offset * 2, 8)) & hex2dec('7fffffff')) + '';
+      otp = (otp).substr(otp.length - 6, 6);
+
+      return otp;
+    }
+
+    function dec2hex(s) { return (s < 15.5 ? '0' : '') + Math.round(s).toString(16); }
+    function hex2dec(s) { return parseInt(s, 16); }
+
+    function base32tohex(base32) {
+      var base32chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+      var bits = "";
+      var hex = "";
+
+      for (var i = 0; i < base32.length; i++) {
+        var val = base32chars.indexOf(base32.charAt(i).toUpperCase());
+        bits += leftpad(val.toString(2), 5, '0');
+      }
+
+      for (var i = 0; i+4 <= bits.length; i+=4) {
+        var chunk = bits.substr(i, 4);
+        hex = hex + parseInt(chunk, 2).toString(16) ;
+      }
+      return hex;
+    }
+
+    function leftpad(str, len, pad) {
+      if (len + 1 >= str.length) {
+        str = Array(len + 1 - str.length).join(pad) + str;
+      }
+      return str;
+    }
+
+  });
