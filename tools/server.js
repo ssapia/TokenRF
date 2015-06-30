c0875d4b23138b4a8fb64366843ad8c522e0249a
salvador <salvador@ssapia.net>
2015-06-30 01:10:29 -0300

    Adiciona no "server" o envio por push.

--- tools/server.js
+++ tools/server.js
@@ -9,10 +9,26 @@ var expressJwt = require('express-jwt');
 
 var jwtSecret = 'fjkdlsajfoew239053/3uk';
 
+////////////////////////////////////////////////////////////////////////////////////////////
+var gcm = require('node-gcm');
+var message = new gcm.Message();
+//API Server Key
+var sender = new gcm.Sender('AIzaSyCan5h5bOdGryRe4Wv9RW_Jwvg4_kwS6YE');
+var registrationIds = [];
+// Value the payload data to send...
+message.addData('message',"<br><b>Mensagem de teste:</b><br><br><p>Essa é uma mensagem de teste.</p>Att.");
+message.addData('title','Teste de push' );
+message.addData('msgcnt','3'); // Shows up in the notification in the status bar
+message.addData('soundname','beep.wav'); //Sound to play upon notification receipt - put in the www folder in app
+//message.collapseKey = 'demo';
+//message.delayWhileIdle = true; //Default is false
+message.timeToLive = 3000;// Duration in seconds to hold in GCM and retry before timing out. Default 4 weeks (2,419,200 seconds) if not specified.
+/////////////////////////////////////////////////////////////////////////////////////////////
+
 var app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use(expressJwt({secret: jwtSecret}).unless({path:['/login']}));
+app.use(expressJwt({secret: jwtSecret}).unless({path:['/login','/notify']}));
 
 var userDB = {
   username: 'admin',
@@ -24,6 +40,15 @@ var userDBRes = {
   username: 'admin',
   tokensecret: 'JBSWY3DPEHPK3PXP'
 };
+
+app.get('/notify', function (req, res) {
+
+   sender.send(message, registrationIds, 4, function (result) {
+    console.log(result);
+  });
+
+  res.status(201).end('Notificacao enviada');
+});
 
 app.post('/login', authenticate, function (req, res) {
   var token = jwt.sign({
@@ -56,12 +81,13 @@ function authenticate(req, res, next) {
   }
 
   console.log('RegID::::::::::');
-  console.log(body.regId);
-/*
-  if (!body.regId) {
-    res.status(401).end('Invalid regId');
+
+  if (body.regId) {
+    registrationIds.push(body.regId);
+    console.log(body.regId);
+  } else {
+    //res.status(401).end('Invalid regId');
   }
-*/
 
   next();
 }
